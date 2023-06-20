354a1baab3e2e780e7e99f3fff7092111f79155b
Asher Ndah <asher.n13.an@gmail.com>
2023-06-20 11:25:11 -0600

    Simple JS rock paper scissors No loop included - still learning CSS and HTML will be added later

--- rpsgame.js
+++ rpsgame.js
@@ -7,22 +7,37 @@ function getComputerChoice (){
 // a round of rock paper scissors
 // parameters playerSelection, computerSelection
 
-
-function playRound (playerSelection, computerSelection) {
-    let playerSelection = prompt('Enter Rock, Paper or, Scissors');
-    if (playerSelection.toLowerCase === 'rock', computerSelection === 'paper') {
-        alert ('You Lose! Paper beats Rock');
-    } else if (playerSelection.toLowerCase === 'rock', computerSelection === 'scissors') {
-        alert ('You Win! Rock beats Scissors');
-    } else if (playerSelection.toLowerCase === 'rock', computerSelection === 'rock'){
-        alert ('Tied game');
-    } else if (playerSelection.toLowerCase !== 'rock') { 
-        alert ('wrong entry');
+    function playRound (computerSelection) {
+        let playerSelection = prompt('Enter Rock, Paper or, Scissors'); 
+        // To use prompt, delete playerSelection parameter - should not be declared twice. 
+        if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'paper') {
+            alert ('You Lose! Paper beats Rock');
+        } else if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'scissors') {
+            alert ('You Win! Rock beats Scissors');
+        } else if (playerSelection.toLowerCase() === 'paper' && computerSelection === 'rock') {
+            alert ('You Win! Paper beats Rock');
+        } else if (playerSelection.toLowerCase() === 'paper' && computerSelection === 'scissors') {
+            alert ('You Lose! Scissors beats Paper');
+        } else if (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'rock') {
+            alert ('You Lose! Rock beats Scissors')
+        } else if (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'paper') {
+            alert ('You Win! Scissors beats Paper')
+        } else if (
+            (playerSelection.toLowerCase() === 'rock' && computerSelection === 'rock') || 
+            (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'scissors') ||
+            (playerSelection.toLowerCase() === 'paper' && computerSelection === 'paper') ) {
+                alert ('Tied game');
+        }  else if (playerSelection.toLowerCase() != 'rock' || 'paper' || 'scissors') { 
+        alert ('Invalid entry');
+        }
     }
-}
 
-const playerSelection ='rock';
+const playerSelection = 'rock' ||'paper' ||'scissors';
 const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+console.log(playRound(computerSelection));
+console.log(playRound(computerSelection));
+console.log(playRound(computerSelection));
+console.log(playRound(computerSelection));
+console.log(playRound(computerSelection));
 
 
